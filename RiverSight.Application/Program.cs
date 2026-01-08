c69759b22342dc8be553e4c59213166f172e8f45
Hilton P Souza <h_iltonsouza@outlook.com>
2026-01-08 18:31:36 -0300

    chore(setup): inicial solution structure, readme and console tester

---
+++ RiverSight.Application/Program.cs
@@ -0,0 +1,41 @@
+﻿// Garantir que esse caminho exista e tenha imagens!
+using RiverSight.Infrastructure.Vision.Services;
+using System.Net.Http.Headers;
+
+string assetsPath = @"C:\RiverSight\Assets";
+
+Console.WriteLine("=== RiverSight Vision Test ===");
+Console.WriteLine($"=== Carregando templates de: {assetsPath}");
+
+// Instancia o nosso leitor de tela
+using var screenReader = new OpenCvScreenReader(assetsPath);
+
+Console.WriteLine("Iniciando loop de detecção. Pressionce Ctrl+C para sair");
+
+while (true)
+{
+    Console.Clear();
+    Console.WriteLine("Lendo tela...");
+
+    // Pede para a Infra ler as cartas do Herói
+    var cards = screenReader.ReadHeroHands();
+
+    if (cards.Any())
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        foreach (var card in cards)
+        {
+            //Deve imprimir "Ah", "Ks", etc.
+            Console.WriteLine($"[DETECTADO] Carta: {card}");
+        }
+    }
+    else
+    {
+        Console.ForegroundColor= ConsoleColor.Red;
+        Console.WriteLine("[vazio] Nenhuma carta identificada nas coordenadas.");
+    }
+
+    Console.ResetColor();
+    Thread.Sleep(500); // 2 FPS é suficiente para teste
+
+}
